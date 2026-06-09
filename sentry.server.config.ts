b70574017834a12ced8832acb722a7acf7045c87
Mayank Thakur <30923904+smayankthakur@users.noreply.github.com>
2026-06-09 11:09:17 +0530

    sentry update

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,8 +1,19 @@
-import * as Sentry from '@sentry/nextjs';
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: process.env.SENTRY_DSN || '',
+  dsn: "https://36fb54e5e919739d2673d0f1693a31e4@o4511533804158976.ingest.us.sentry.io/4511533805731840",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
-  debug: false,
-  enabled: !!process.env.SENTRY_DSN,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
 });
